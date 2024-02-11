9c1479ec3f30107ff624512c80e263bd6142c212
ale-aleksandra <aleksandra99j@gmail.com>
2024-02-11 10:35:48 +0000

    Add GitHub link

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import logo from "./logo.png";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <footer>
+          This project was coded by Aleksandra Jablonska and is open-sourced on{" "}
+          <a
+            href="https://github.com/ale-aleksandra/dictionary-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
