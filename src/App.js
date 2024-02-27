5fd5f8f2e99f67d5dabfd15c3c5f6429fdd25da0
ale-aleksandra <aleksandra99j@gmail.com>
2024-02-27 10:25:29 +0000

    Update css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import logo from "./logo.png";
 import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
+        <header className="App-header">Dictionary</header>
         <main>
           <Dictionary defaultKeyword="programming" />
         </main>
